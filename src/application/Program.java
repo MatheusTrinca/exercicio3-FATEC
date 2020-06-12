f1a079bb3c62bc34d0bf9469ab2f2334f8237f0e
Matheus Felipe Trinca <matheusfelipetrinca@gmail.com>
2020-06-11 23:02:03 -0300

    segundo commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,3 +1,5 @@
+// Matheus Felipe Trinca - João Vitor Pereira
+
 package application;
 
 import java.text.ParseException;
@@ -16,13 +18,14 @@ public class Program {
 
 		while(true) {
 			System.out.println();
-			System.out.println("DESPESAS");
+			System.out.println("=========== DESPESAS ===========");
 			System.out.println("1.Cadastrar ou Adicionar Despesa\n"
 							  +"2.Listar todas as Despesas\n"
 							  +"3.Consulta por determinada despesa\n"
 							  +"4.Imprimir total de despesas\n"
 							  +"5.Imprimir despesas por categoria\n"
 							  +"6.Finalizar");
+			System.out.println("=================================");
 			op = sc.nextInt();
 			if (op < 1 || op > 6) {
 				System.out.println("Valor Digitado inválido - Digite Novamente");
@@ -36,8 +39,10 @@ public class Program {
 			char opSub;
 			while(true) {
 				System.out.println("3.Consulta por determinada despesa\n"
+								  +"   ===============\n"
 								  +"   a.Por descrição\n"
-								  +"   b.Por tipo");
+								  +"   b.Por tipo\n"
+								  +"   ===============\n");
 				opSub = sc.next().toLowerCase().charAt(0);
 				if(opSub == 'a') {
 					op = 7;
@@ -65,7 +70,7 @@ public class Program {
 	public static double getTotalCategoria(List<Despesa> despesas, String categoria) {
 		double soma = 0;
 		for(Despesa despesa : despesas) {
-			if(despesa.getTipo() == categoria) {
+			if(despesa.getTipo().equals(categoria)) {
 				soma += despesa.getValor();
 			}
 		}
@@ -73,6 +78,7 @@ public class Program {
 	}
 	
 	// Busca na descrição do objeto a palavra digitada no menu, e retorna a despesa, se encontrada.
+	// Considerando que a descrição seja um breve texto, frase ou palavra.
 	public static List<Despesa> buscaPalavra(List<Despesa> despesas, String palavra) {
 		List<Despesa> achados = new ArrayList<>();
 		int tamanhoPalavra = palavra.length();
@@ -88,10 +94,8 @@ public class Program {
 		}
 		return achados;
 	}
-	
-	
 
-
+	
 	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
@@ -105,30 +109,35 @@ public class Program {
 			op = menu();
 			switch (op) {
 			case 1:
-				System.out.println("Quantas despesas deseja cadastrar?");
-				int qtdDespesa = sc.nextInt();
-				for(int i = 0; i < qtdDespesa; i++) {
+				while(true) {
 					System.out.println("Cadastro de despesa #"+idDespesa);
-					sc.nextLine();
 					System.out.print("Digite a descrição: ");
 					String descricao = sc.nextLine();
 					System.out.print("Digite o tipo: ");
 					String tipo = sc.nextLine();
 					System.out.print("Valor: ");
 					double valor = sc.nextDouble();
-					System.out.println("Data(DD/MM/AAAA): ");
+					System.out.print("Data(DD/MM/AAAA): ");
 					Date data = sdf.parse(sc.next());
 					despesas.add(new Despesa(idDespesa, descricao, tipo, valor, data));
-					idDespesa += qtdDespesa;
+					idDespesa ++;
+					System.out.println("Deseja novo cadastro? (S/N)");
+					char novo = sc.next().toLowerCase().charAt(0);
+					if(novo == 'n') 
+						break;
 				}
 				break;
 			case 2:
 				if(!despesas.isEmpty()) {
 					for(Despesa despesa : despesas) 
 						System.out.println(despesa);
+						sc.nextLine();
 				}else {
 					System.out.println("Cadastre ao menos 1 despesa");
 				}
+				System.out.println();
+				System.out.println("Aperte qualquer tecla para voltar ao menu inicial");
+				sc.nextLine();
 				break;
 			case 4:
 				if(!despesas.isEmpty()) {
@@ -136,42 +145,64 @@ public class Program {
 				}else {
 					System.out.println("Cadastre ao menos 1 despesa");
 				}
+				System.out.println();
+				System.out.println("Aperter quaquer caractere para voltar ao menu inicial");
+				sc.nextLine();
 				break;
 			case 5:
-				if(despesas.isEmpty()) {
+				if(!despesas.isEmpty()) {
+					System.out.println("Digite a categoria");
+					String categoria = sc.next();
+					double total = getTotalCategoria(despesas, categoria);
+					if(total == 0) {
+						System.out.println("Não existem despesas ou a categoria selecionada é inválida");
+						sc.nextLine();
+					}else
+						System.out.println("Total de despesas de "+categoria+" R$"+String.format("%.2f",total));
+						sc.nextLine();
+				}else {
 					System.out.println("Cadastre ao menos 1 despesa");
 				}
-				System.out.println("Digite a categoria");
-				String categoria = sc.next();
-				double total = getTotalCategoria(despesas, categoria);
-				if(total == 0) {
-					System.out.println("Não existem despesas ou a categoria selecionada é inválida");
-				}else 
-					System.out.println("Total de despesas de "+categoria+" R$"+String.format("%.2f",total));
-			
-			
-			case 7:
-				String palavra;
-				while(true) {
-					// considera palavras acima de 4 letras;
-					System.out.println("Digite uma palavra para a busca");
-					palavra = sc.next();
-					int tamanhoPalavra = palavra.length();
-					if(tamanhoPalavra < 4) {
-						System.out.println("Palavra inválida, digite palavras com o minimo de 5 caracteres");
-					}else {
-						break;
-					}
-				}
-				if(buscaPalavra(despesas, palavra).isEmpty()) {
-					System.out.println("Não foram achadas descrições relacionadas");
-				}else {
-					System.out.println("Despesas encontradas");
-					for(Despesa despesa : buscaPalavra(despesas, palavra)) {
-						System.out.println(despesa);
-					}
-				}
+				System.out.println();
+				System.out.println("Aperter quaquer caractere para voltar ao menu inicial");
+				sc.nextLine();
 				break;
+			case 7:
+				if(!despesas.isEmpty()) {
+					String palavra;
+					while(true) {
+						// considera palavras acima de 4 letras;
+						System.out.println("Digite uma palavra para a busca");
+						palavra = sc.next();
+						int tamanhoPalavra = palavra.length();
+						if(tamanhoPalavra < 4) {
+							System.out.println("Palavra inválida, digite palavras com o minimo de 4 caracteres");
+						}else {
+							break;
+						}
+					}
+					if(buscaPalavra(despesas, palavra).isEmpty()) {
+						System.out.println("Não foram achadas descrições relacionadas");
+					}else {
+						System.out.println("Despesas encontradas");
+						for(Despesa despesa : buscaPalavra(despesas, palavra)) {
+							System.out.println(despesa);
+							System.out.println();
+						}
+						sc.nextLine();
+					}
+				}else {
+					System.out.println("Cadastre ao menos 1 despesa");
+				}
+				System.out.println();
+				System.out.println("Aperter quaquer caractere para voltar ao menu inicial");
+				sc.nextLine();
+				break;
+			case 8:
+				/* MOSTRAR OS OBJETOS da categoria/tipo que a pessoa digitar
+				 * AQUI VC PERGUNTA
+				 * */
+				
 			}
 		}while(op != 6);
 		
